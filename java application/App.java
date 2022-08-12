2362ce3626a8cb0bddd056aa7bf8c67f3bf75834
Simthe <106746694+simthembilekleinbooi@users.noreply.github.com>
2022-08-12 10:32:58 +0200

    Add files via upload

---
+++ java application/App.java
@@ -0,0 +1,7 @@
+public class App
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Mihlali, this is my first Java application!");
+        System.out.println("I hope you like it :)");
+    }
+}
